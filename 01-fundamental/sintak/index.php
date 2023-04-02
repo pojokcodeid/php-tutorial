47b5d523b7cbf00b19aa4254c096f38751607338
Asep Komarudin <68836805+pojokcodeid@users.noreply.github.com>
2023-04-02 21:10:53 +0700

    Add files via upload

---
+++ 01-fundamental/sintak/index.php
@@ -0,0 +1,25 @@
+<?php
+echo "Hello World!";
+
+$nilai = 1;
+
+IF ($nilai == 1) {
+    echo "benar";
+} ELSE {
+    echo "salah";
+}
+
+CLASS Coba{
+
+
+    public FUNCTION                             coba(){
+
+
+        echo "ini adalah class Coba";
+    }
+
+
+}
+
+$coba = new Coba();
+$coba->coba();
