b650a3a9b5266868b0084cf390e9d6ab98bbe0b5
asep komarudin <pojokcodeid@gmail.com>
2023-04-19 04:41:04 +0700

    update

--- 04-Control-flow/controlif.php
+++ 04-Control-flow/controlif.php
@@ -3,15 +3,15 @@
 # if dengan satu statement
 $is_admin = true;
 if ($is_admin)
-    echo "Anda adalah admin";
+  echo "Anda adalah admin";
 echo "<br>";
 
 #if dengan control block
 $can_edit = false;
 $is_admin = true;
 if ($is_admin) {
-    echo "Anda adalah admin";
-    $can_edit = true;
+  echo "Anda adalah admin";
+  $can_edit = true;
 }
 
 echo "<br>";
@@ -19,10 +19,10 @@ echo "<br>";
 $is_admin = true;
 $can_approve = false;
 if ($is_admin) {
-    echo "Anda adalah admin<br>";
-    if ($can_approve) {
-        echo "Anda dapat melakukan approve";
-    }
+  echo "Anda adalah admin<br>";
+  if ($can_approve) {
+    echo "Anda dapat melakukan approve";
+  }
 }
 echo "<br>";
 ?>
@@ -31,22 +31,22 @@ echo "<br>";
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
 </head>
 
 <body>
-    <?php
-    $is_admin = true;
-    if ($is_admin):
-        ?>
-        <a href="#">Edit</a><br>
-        <?php
-    endif;
-    ?>
-    <a href="#">View</a>
+  <?php
+  $is_admin = true;
+  if ($is_admin) :
+  ?>
+    <a href="#">Edit</a><br>
+  <?php
+  endif;
+  ?>
+  <a href="#">View</a>
 </body>
 
 </html>
@@ -55,9 +55,9 @@ echo "<br>";
 # kesalahan sering terjadi untuk if
 $checed = "on";
 if ($checed == 'off') {
-    echo "check bos tidak dipilih";
+  echo "check bos tidak dipilih";
 }
 
 if ('on' == $checed) {
-    echo "check bos dipilih";
+  echo "check bos dipilih";
 }
