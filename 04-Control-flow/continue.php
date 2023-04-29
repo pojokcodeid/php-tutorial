e2299fabd4c1634abad449539e04e30374f35990
asep komarudin <pojokcodeid@gmail.com>
2023-04-29 11:58:15 +0700

    update

--- 04-Control-flow/continue.php
+++ 04-Control-flow/continue.php
@@ -4,23 +4,23 @@ for ($i = 1; $i <= 10; $i++) {
     if ($i % 2 == 0) {
         continue;
     }
-    echo $i . " ";
+    echo $i . "<br>";
 }
-echo '<hr>';
+
+echo "<hr>";
 
 $students = [
     ['name' => 'John', 'score' => 80],
-    ['name' => 'Jane', 'score' => 90],
-    ['name' => 'Jim', 'score' => 70],
+    ['name' => 'Mary', 'score' => 90],
+    ['name' => 'Peter', 'score' => 70],
     ['name' => 'Josh', 'score' => 85],
-    ['name' => 'Jack', 'score' => 60]
+    ['name' => 'Jim', 'score' => 60],
+    ['name' => 'Jack', 'score' => 75],
 ];
 
 foreach ($students as $student) {
-    // Skip students with scores below 75
     if ($student['score'] < 75) {
         continue;
     }
-
-    echo $student['name'] . " passed with a score of " . $student['score'] . "<br>";
+    echo $student['name'] . ' - ' . $student['score'] . "<br>";
 }
