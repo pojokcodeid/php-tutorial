73e50c74b76f9ce87ffd2f866d62f8477eb6dfaf
asep komarudin <pojokcodeid@gmail.com>
2023-07-29 06:49:25 +0700

    update

--- 62-mvc/src/config/default.php
+++ 62-mvc/src/config/default.php
@@ -1,8 +1,20 @@
 <?php 
 
-define('BASEURL', 'http://localhost/php-tutorial/62-mvc');
+// read from file properties
+$file="../src/config/config.properties";
+if (!file_exists($file)) {
+  echo "Config file tidak ada, silahkan isi config src/config/config.properties";
+  exit;
+}
+$properties = parse_ini_file($file);
 
-define('DB_HOST', 'localhost');
-define('DB_USER', 'root');
-define('DB_PASS', '');
-define('DB_NAME', 'latihan');
+define('BASEURL', $properties['base_url']);
+
+define('DB_HOST', $properties['db_host']);
+define('DB_USER', $properties['db_user']);
+define('DB_PASS', $properties['db_pass']);
+define('DB_NAME', $properties['db_name']);
+
+if($properties['migrate']){
+  require_once '../src/config/migrate.php';
+}
---
+++ 62-mvc/src/config/migrate.php
@@ -0,0 +1,5 @@
+<?php 
+
+// file ini digunakan untuk migrasi database pertama kali
+
+echo 'SEKARANG MODE MIGRASI ACTIVE';
