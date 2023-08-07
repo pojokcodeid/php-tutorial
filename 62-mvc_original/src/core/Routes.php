9be24543debd7a8c34672849e108679a0ddf7bca
asep komarudin <pojokcodeid@gmail.com>
2023-08-07 13:49:49 +0700

    update

--- 62-mvc_original/src/core/Routes.php
+++ 62-mvc_original/src/core/Routes.php
@@ -12,7 +12,7 @@ class Routes
     // $router->get('/',['DefaultApp','index']);
     $router->get('/barang', ['Barang', 'index']);
     $router->get('/barang/insert', ['Barang', 'insert']);
-    $router->get('/barang/edit', ['Barang', 'edit']);
+    $router->get('/barang/edit/:id', ['Barang', 'edit']);
     $router->post('/barang/insert_barang', ['Barang', 'insert_barang']);
     $router->post('/barang/edit_barang', ['Barang', 'edit_barang2']);
 
