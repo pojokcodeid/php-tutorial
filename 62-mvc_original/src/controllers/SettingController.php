64d037c8d90c06bd90769ede816dcc29b9a60246
asep komarudin <pojokcodeid@gmail.com>
2023-08-07 17:10:58 +0700

    update

--- 62-mvc_original/src/controllers/SettingController.php
+++ 62-mvc_original/src/controllers/SettingController.php
@@ -1,11 +1,14 @@
-<?php 
+<?php
+use MyApp\Core\BaseController;
 
-class SettingController extends BaseController{
-  public function index(){
-    $data=[
-      'judul'=>'Setting',
+class SettingController extends BaseController
+{
+  public function index()
+  {
+    $data = [
+      'judul' => 'Setting',
     ];
-    $this->view('template/header',$data);
+    $this->view('template/header', $data);
     $this->view('setting/index');
     $this->view('template/footer');
   }
