2cc34f40baf878848796fe8b79368034ed4c4c96
asep komarudin <pojokcodeid@gmail.com>
2023-08-14 06:00:07 +0700

    update

--- 65-read-api/src/refresh.php
+++ 65-read-api/src/refresh.php
@@ -1,27 +1,27 @@
 <?php
 
-$curl = curl_init();
+// $curl = curl_init();
 
-curl_setopt_array($curl, [
-  CURLOPT_URL => "http://localhost/php-tutorial/63-API/refresh",
-  CURLOPT_RETURNTRANSFER => true,
-  CURLOPT_ENCODING => "",
-  CURLOPT_MAXREDIRS => 10,
-  CURLOPT_TIMEOUT => 30,
-  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-  CURLOPT_CUSTOMREQUEST => "GET",
-  CURLOPT_HTTPHEADER => [
-    "Accept: */*",
-  ],
-]);
+// curl_setopt_array($curl, [
+//   CURLOPT_URL => "http://localhost/php-tutorial/63-API/refresh",
+//   CURLOPT_RETURNTRANSFER => true,
+//   CURLOPT_ENCODING => "",
+//   CURLOPT_MAXREDIRS => 10,
+//   CURLOPT_TIMEOUT => 30,
+//   CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+//   CURLOPT_CUSTOMREQUEST => "GET",
+//   CURLOPT_HTTPHEADER => [
+//     "Accept: */*",
+//   ],
+// ]);
 
-$response = curl_exec($curl);
-$err = curl_error($curl);
+// $response = curl_exec($curl);
+// $err = curl_error($curl);
 
-curl_close($curl);
+// curl_close($curl);
 
-if ($err) {
-  echo "cURL Error #:" . $err;
-} else {
-  echo $response;
-}
+// if ($err) {
+//   echo "cURL Error #:" . $err;
+// } else {
+//   echo $response;
+// }
