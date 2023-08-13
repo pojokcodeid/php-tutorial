def98a5724630b1180f4e9017ff6a89376639fe3
asep komarudin <pojokcodeid@gmail.com>
2023-08-14 05:57:08 +0700

    update

--- 65-read-api/src/update.php
+++ 65-read-api/src/update.php
@@ -6,7 +6,7 @@ $harga = isset($_POST['harga']) ? $_POST['harga'] : '';
 $kadaluarsa = isset($_POST['kadaluarsa']) ? $_POST['kadaluarsa'] : '';
 
 include 'validate.php';
-
+// memanggil fungsi curl
 $curl = curl_init();
 $header = [
   "Accept: */*",
@@ -21,20 +21,30 @@ $data = '{
 }';
 
 curl_setopt_array($curl, [
+  // menenukan url yang akan diacess
   CURLOPT_URL => "http://localhost/php-tutorial/63-API/barang/$id",
+  // menentukan apakah curl akan mengembalikan data dalam bentuk string
   CURLOPT_RETURNTRANSFER => true,
+  // menentukan jenis encoding yang akan digunakan                                  
   CURLOPT_ENCODING => "",
+  // menentukan jumlah maksimum redirect yang akan diizinkan                                         
   CURLOPT_MAXREDIRS => 10,
+  // menentukan waktu timeout yang akan diizinkan                                        
   CURLOPT_TIMEOUT => 30,
+  // menentukan versi http yang akan digunakan                                            
   CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+  // menentukan metode request yang akan diakses PUT POST, GET dst..                   
   CURLOPT_CUSTOMREQUEST => "PATCH",
+  // menentukan data yang akan diakses                                
   CURLOPT_POSTFIELDS => $data,
+  // menentukan header yang akan digunakan                                     
   CURLOPT_HTTPHEADER => $header,
 ]);
-
+// menjalankan curl
 $response = curl_exec($curl);
+// menentukan error                                    
 $err = curl_error($curl);
-
+// menutup curl
 curl_close($curl);
 
 if ($err) {
